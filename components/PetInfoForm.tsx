834352dc6c92991ad3886f2d086a25b0d29889c5
Yonguk Lee <qjrmsktso2@gmail.com>
2024-04-03 20:59:11 +0900

    implement name, breed, birth, sex, menstruation of Form

--- components/PetInfoForm.tsx
+++ components/PetInfoForm.tsx
@@ -1,32 +1,94 @@
 "use client";
 
-import { PetInfo } from "@/types/PetInfo";
-import { Button, Form, Input } from "antd";
+import { Breed, PetInfo, Sex } from "@/types/PetInfo";
+import { Button, Checkbox, DatePicker, Form, Input, Select } from "antd";
+import { Dayjs } from "dayjs";
+import { useState } from "react";
 
 export default function PetInfoForm({
   onSubmit,
 }: {
   onSubmit: (pet: PetInfo) => void;
 }) {
+  const [sex, setSex] = useState<Sex>();
+
+  const onFinish = (
+    pet: PetInfo & {
+      birth: Dayjs;
+    }
+  ) => {
+    const payload: PetInfo = {
+      ...pet,
+      birth: pet.birth.format("YYYY-MM"),
+    };
+    console.log(payload);
+
+    onSubmit(payload);
+  };
+
   return (
     <Form
       name="basic"
       labelCol={{ span: 8 }}
       wrapperCol={{ span: 16 }}
       initialValues={{ remember: true }}
-      onFinish={(pet) => {
-        onSubmit(pet);
-        console.log("pet", pet);
-      }}
+      onFinish={onFinish}
       autoComplete="off"
     >
       <Form.Item<PetInfo>
-        label="이름"
+        label="강아지 이름"
         name="name"
-        rules={[{ required: true, message: "Please input your username!" }]}
+        rules={[{ required: true, message: "강아지 이름을 선택해 주세요" }]}
       >
         <Input />
       </Form.Item>
+
+      <Form.Item<PetInfo>
+        label="견종"
+        name="breed"
+        rules={[{ required: true, message: "견종을 선택해주세요" }]}
+      >
+        <Select
+          showSearch
+          options={Object.entries(Breed).map(([label, value]) => ({
+            label,
+            value,
+          }))}
+        />
+      </Form.Item>
+
+      <Form.Item<PetInfo>
+        label="생년월"
+        name="birth"
+        rules={[{ required: true, message: "생년월을 선택해주세요" }]}
+      >
+        <DatePicker picker="month" />
+      </Form.Item>
+
+      <Form.Item<PetInfo>
+        label="성별"
+        name="sex"
+        rules={[{ required: true, message: "성별을 선택해주세요" }]}
+      >
+        <Select
+          value={sex}
+          onChange={setSex}
+          options={Object.entries(Sex).map(([label, value]) => ({
+            label,
+            value,
+          }))}
+        />
+      </Form.Item>
+
+      {sex === Sex.암 && (
+        <Form.Item<PetInfo>
+          label="생리중"
+          name="menstruation"
+          valuePropName="checked"
+        >
+          <Checkbox />
+        </Form.Item>
+      )}
 
       <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
         <Button type="primary" htmlType="submit">
--- types/PetInfo.ts
+++ types/PetInfo.ts
@@ -1,3 +1,44 @@
+export enum Breed {
+  푸들 = "푸들",
+  말티즈 = "말티즈",
+  시츄 = "시츄",
+  요크셔테리어 = "요크셔테리어",
+  포메라니안 = "포메라니안",
+}
+
+export enum AllergySource {
+  소스1 = "소스1",
+  소스2 = "소스2",
+  소스3 = "소스3",
+  소스4 = "소스4",
+  소스5 = "소스5",
+}
+
+export enum ProteinSource {
+  소스1 = "소스1",
+  소스2 = "소스2",
+  소스3 = "소스3",
+  소스4 = "소스4",
+  소스5 = "소스5",
+}
+
+export enum Sex {
+  수 = "male",
+  암 = "female",
+  중성화 = "neutered",
+}
+
 export interface PetInfo {
   name: string;
+  breed: Breed | string;
+  birth: string; // YYYY-MM
+  sex: "male" | "female" | "neutered";
+  menstruation?: boolean;
+  pregnancy?: boolean;
+  weight: number;
+  allergy: boolean;
+  allergySource?: AllergySource;
+  protein: boolean;
+  proteinSource?: ProteinSource;
+  // TODO: add health problems
 }
