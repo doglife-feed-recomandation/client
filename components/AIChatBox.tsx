a115a88742d6a41fc275b0146d2ba41a1785da30
ksah3756 <ksah3756@gmail.com>
2024-05-20 11:40:49 +0900

    Add example message

--- components/AIChatBox.tsx
+++ components/AIChatBox.tsx
@@ -3,31 +3,47 @@
 import { createChatLog } from '@/actions/chat';
 import { Input } from '@/components/Input';
 import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { IconUser } from '@/components/ui/icons';
 import { cn } from '@/lib/utils';
 import { Message, useChat } from 'ai/react';
 import { useEffect, useRef } from 'react';
+import { ulid } from 'ulid';
 
 interface AiChatBoxProps {
   initialMessages: Message[];
 }
 
+const exampleMessages = [
+  '이 중에서 한가지만 추천해주세요.',
+  '사료는 어느정도를 주는게 좋을까요?',
+  '사료 교체 주기는 어느 정도가 적당한가요?',
+  '저번에 사용한 사료라서 다른 사료를 추천해 주세요.',
+];
+
 export default function AIChatBox({ initialMessages }: AiChatBoxProps) {
-  const { messages, input, handleInputChange, handleSubmit, isLoading, error } =
-    useChat({
-      initialMessages,
-      initialInput: '어떤 사료가 좋을까요?',
-      onFinish(message) {
-        const assistantMessage = {
-          role: message.role,
-          content: message.content,
-          id: message.id,
-          createdAt: Date.now().toString(),
-        };
-        createChatLog(assistantMessage);
-        console.log(assistantMessage);
-      },
-    });
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    isLoading,
+    error,
+    append,
+  } = useChat({
+    initialMessages,
+    initialInput: '어떤 사료가 좋을까요?',
+    onFinish(message) {
+      const assistantMessage = {
+        role: message.role,
+        content: message.content,
+        id: message.id,
+        createdAt: Date.now().toString(),
+      };
+      createChatLog(assistantMessage);
+      console.log(assistantMessage);
+    },
+  });
 
   const inputRef = useRef<HTMLInputElement>(null);
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -40,9 +56,29 @@ export default function AIChatBox({ initialMessages }: AiChatBoxProps) {
 
   const lastMessageIsUser = messages[messages.length - 1]?.role === 'user';
 
+  const handleExampleClick = (example: string) => {
+    const newMessage: Message = { id: ulid(), content: example, role: 'user' };
+    append(newMessage);
+  };
+
   return (
     <>
       <div className="mt-3 h-full overflow-y-auto px-3" ref={scrollRef}>
+        {messages.length == 1 && (
+          <div className="mb-4 grid grid-cols-2 gap-4 grid-flow-row px-4 sm:px-0">
+            {exampleMessages.map((message, index) => (
+              <Card
+                key={message}
+                className={`p-4 hover:bg-zinc-100 ${
+                  index > 1 && 'hidden md:block'
+                }`}
+                onClick={() => handleExampleClick(message)}
+              >
+                {message}
+              </Card>
+            ))}
+          </div>
+        )}
         {messages.slice(1).map((message) => (
           <ChatMessage message={message} key={message.id} />
         ))}
